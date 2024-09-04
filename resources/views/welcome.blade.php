e64216e9e754d83df8e9e3cb914d3e40f57d762e
Matheus Alves <matheusalvessilvabr@gmail.com>
2024-09-04 16:05:54 -0300

    fix search butons Closes #7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,15 +29,15 @@
                 <div class="flex justify-center">
                    <h1 class="text-white">Awesome Ideas</h1>
                 </div>
-                <p class="mt-5 text-white">Search for repos in github with awesome ideas <br> and awesome results....</p>
+                <p class="mt-5 text-white">Search for repos in github with awesome ideas and awesome results....</p>
 
                 <div class="mt-16">
                     <div class="relative flex">
-                        <form action={{ route('searchRepositories') }} method="GET">
+                        <form class="relative flex" {{ route('searchRepositories') }} method="GET">
                             <input
                             name="search"
                             type="search"
-                            class="relative m-0 -me-0.5 block flex-auto rounded-s border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:text-white dark:placeholder:text-neutral-200 dark:autofill:shadow-autofill dark:focus:border-primary"
+                            class="input__size relative m-0 -me-0.5 block flex-auto rounded-s border border-solid border-neutral-200 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-neutral-500 focus:z-[3] focus:border-primary focus:shadow-inset focus:outline-none motion-reduce:transition-none dark:border-white/10 dark:text-white dark:placeholder:text-neutral-200 dark:autofill:shadow-autofill dark:focus:border-primary"
                             placeholder="Search for a Awesome repo.."
                             aria-label="Search"
                             id="exampleFormControlInput3"
